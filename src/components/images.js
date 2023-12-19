8d57252eac31f7608d26d698664a613ac4e3f0f1
Yaroslav Kobylko <kobylkoyaroslav@gmail.com>
2023-12-20 00:36:30 +0200

    Commit

---
+++ src/components/images.js
@@ -0,0 +1,7 @@
+import BeerLogo from './LogoBeer.png';
+import BeerTopic from './BeerTopic.jpg';
+import LR from './LR.jpg';
+import FormImage from './FormImage.png';
+
+
+export { BeerLogo, BeerTopic, LR, FormImage};
